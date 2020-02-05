06dcebb6da0f2a9c94cb9096285e912cb7bbafdd
Duncuiad <36989446+Duncuiad@users.noreply.github.com>
2020-02-05 21:23:21 +0100

    Creata l'intestazione per Strumento

--- Strumento.java
+++ Strumento.java
@@ -1,3 +1,10 @@
 public abstract class Strumento {
-  
+
+  private Canvas canvas;
+
+  public Strumento(Canvas canvas) {
+    this.canvas = canvas;
+  }
+
+  public abstract void ricevi(EventoDiMouse e);
 }
