7250fd3b21b7af75eeb315befcf7d7d9acc55e99
Duncuiad <36989446+Duncuiad@users.noreply.github.com>
2020-02-05 16:16:40 +0100

    Aggiornato StrumentoDiDisegno

--- StrumentoDiDisegno.java
+++ StrumentoDiDisegno.java
@@ -1,7 +1,17 @@
+/** Le istanze di questa classe rappresentano
+ *
+ *
+ */
+
 public abstract class StrumentoDiDisegno extends Strumento {
 
+  /** Questo &egrave; il tratto di disegno corrente, comune a tutte le istanze di StrumentoDiDisegno */
   private static char trattoDiDisegno = '*'
 
+  /** Modifica il tratto di disegno
+   *
+   * @param c Il carattere che si vuole utilizzare
+   */
   public static void setTratto(char c) {
     this.trattoDiDisegno = c;
   }
