6e643d223cb26ce45217333128a21fba295300ad
Duncuiad <36989446+Duncuiad@users.noreply.github.com>
2020-02-05 21:44:53 +0100

    Creata l'intestazione per Strumento

--- Strumento.java
+++ Strumento.java
@@ -1,10 +1,31 @@
+/** Questa classe astratta &egrave; la superclasse (diretta o indiretta) di tutti gli strumenti
+ *
+ *
+ */
 public abstract class Strumento {
 
+  /** Questo &egrave; il riferimento al canvas su cui opera uno Strumento */
   private Canvas canvas;
 
+  //COSTRUTTORI
+  /** Associa allo Strumento il canvas su cui agire
+   *
+   * @param canvas riferimento al canvas su cui operare
+   */
   public Strumento(Canvas canvas) {
     this.canvas = canvas;
   }
 
+  /** Il costruttore senza parametri inizializza this.canvas a null */
+  public Strumento() {
+    this(null);
+  }
+
+  //METODI
+  /** Prototipo del metodo ricevi(EventoDiMouse e), da ridefinire nelle sottoclassi di Strumento
+   *
+   * @param e l'EventoDiMouse a cui lo Strumento deve rispondere
+   */
   public abstract void ricevi(EventoDiMouse e);
+
 }
