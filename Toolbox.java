bd7c77d0b848f26e360a66a44bf321cf6bc35e2c
Duncuiad <36989446+Duncuiad@users.noreply.github.com>
2020-02-06 17:13:49 +0100

    Implementato Toolbox (dipende da Canvas.undo(int k), da implementare) Implementato EventoDiTastiera

--- Toolbox.java
+++ Toolbox.java
@@ -1,3 +1,97 @@
+import java.util.*; //Map<K,V>, HashMap<K,V>
+
+/** Insieme di strumenti, ciascuno associato a un carattere in modo univoco, e in cui al pi� uno strumento � in ogni istante attivo
+ *
+ *
+ */
 public class Toolbox {
+
+  //ATTRIBUTI
+  /** Il canvas su cui agiscono gli strumenti della toolbox */
+  private Canvas canvas;
+  /** Il dizionario che associa a ogni etichetta uno strumento */
+  private HashMap<Character,Strumento> strumenti = null;
+  /** Lo strumento attualmente selezionato */
+  private Strumento attivo = null;
+
+  //COSTRUTTORI
+  /** Associa alla toolbox il suo canvas e inizializza la lista degli strumenti
+   *
+   * @param canvas il canvas su cui la toolbox deve agire
+   */
+  public Toolbox(Canvas canvas) {
+    this.canvas = canvas;
+    strumenti = new HashMap<Character,Strumento>(); // crea una HashMap inizialmente lunga 16 elementi, che viene espansa dopo i primi 12 (loading factor 0.75)
+  }
+
+  //METODI
+  /** Aggiunge uno strumento alla toolbox
+   *
+   * @param s lo strumento da aggiungere
+   * @param c l'etichetta con cui si vuole identificare lo strumento
+   */
+  public void aggiungi(Strumento s, char c) {
+	  if (strumenti.containsKey(c)) { // se c e' gia' l'etichetta di uno strumento, sovrascrivi lo strumento precedente
+		  strumenti.replace(c, s);
+	  }
+	  else {
+		  strumenti.put(c, s); //autoboxing di c
+	  }
+  }
+
+  /** Riceve un evento e agisce di conseguenza
+   * 1. se l'evento � di tastiera (� un carattere):
+   * 	1.1 se il tasto premuto identifica uno strumento, cambia lo strumento
+   * 	1.2 se invece non identifica uno strumento ma si � premuto il tasto "u", annulla l'ultima azione sul canvas
+   * 	1.3 se invece non � nulla di tutto ci�, e lo strumento attivo � di disegno, usa il carattere come tratto
+   * 2. se l'evento � di mouse (� un clic) e c'� uno strumento attivo, passalo a quello strumento
+   *
+   * @param e l'evento registrato
+   */
+  public void ricevi(Evento e) {
+	  if (e instanceof EventoDiTastiera) { // 1. caso tastiera
+		  EventoDiTastiera eTast = (EventoDiTastiera) e;
+		  char tastoPremuto = eTast.car();
+		  if (strumenti.containsKey(tastoPremuto)) { // 1.1 se il tasto indica uno strumento nel toolbox
+			  this.attivo = strumenti.get(tastoPremuto); // cambia strumento attivo
+		  }
+		  else if (tastoPremuto == 'u') { // 1.2 se 'u' non � nel toolbox
+				  canvas.undo(1); // torna indietro di un'azione
+			  }
+		  else if (this.attivo instanceof StrumentoDiDisegno) { // 1.3 se lo strumento attivo � di disegno
+					  StrumentoDiDisegno.setTratto(tastoPremuto); // cambia il tratto
+				  }
+	  }
+	  else if ((e instanceof EventoDiMouse) && (this.attivo != null)) { // 2. caso mouse
+		  EventoDiMouse eMouse = (EventoDiMouse) e;
+		  this.attivo.ricevi(eMouse); // passa l'evento allo strumento attivo
+	  }
+  }
+
+  /** Restituisce la lista degli strumenti come mappa tra le etichette e gli strumenti
+   *
+   * @return la mappa (etichetta, strumento)
+   */
+  public Map<Character,Strumento> strumenti() {
+	  return strumenti;
+  }
+
+  /** Restituisce lo strumento attualmente selezionato
+   *
+   * @return lo strumento attualmente selezionato
+   */
+  public Strumento attivo() {
+    return attivo;
+  }
+
+  /** Se il carattere passato identifica uno strumento nella toolbox, rende attivo tale strumento
+   *
+   * @param c l'etichetta dello strumento da attivare
+   */
+  public void attiva(char c) {
+	  if (strumenti.containsKey(c)) {
+		  this.attivo = strumenti.get(c);
+	  }
+  }
 
 }
