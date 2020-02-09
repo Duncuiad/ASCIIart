cc0a6f0300e99c6cb2736da0ce901d0598d4ed14
patriziafornoni <60657220+patriziafornoni@users.noreply.github.com>
2020-02-09 18:00:40 +0100

    flipHoriz

--- FlipHoriz.java
+++ FlipHoriz.java
@@ -6,12 +6,22 @@ public class FlipHoriz extends StrumentoDiSelezione {
   }
 
   //METODI
-  public void ricevi(EventoDiMouse e) {
-
-  }
 
   public void azione() {
-
+	  int h = Math.abs(pIniziale.posx() - pFinale.posx());
+	  int l = Math.abs(pIniziale.posy() - pIniziale.posy());
+	  
+	  int x0 = Math.min(pIniziale.posx(), pFinale.posx());
+	  int y0 = (int) Math.round(l / 2.0);
+	  
+	  char c;
+	  
+	  for(int i=0; i<h; i++) {
+		  for(int j=0; j<y0; j++) {
+			  c=canvas.car(x0+i, y0+j);
+			  canvas.modifica(x0+i, y0+j, canvas.car(x0 + i, y0 - j));
+			  canvas.modifica(x0+i, y0-j, c);
+			  }
+		  }
+	  }
   }
-
-}
