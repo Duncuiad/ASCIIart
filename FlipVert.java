ac1af917b06f8da2bb0bf14e8b77c11254a18d99
patriziafornoni <60657220+patriziafornoni@users.noreply.github.com>
2020-02-09 18:04:05 +0100

    flipVert

--- FlipVert.java
+++ FlipVert.java
@@ -6,12 +6,24 @@ public class FlipVert extends StrumentoDiSelezione {
   }
 
   //METODI
-  public void ricevi(EventoDiMouse e) {
 
+  public void azione() {	  
+  int h = Math.abs(pIniziale.posx() - pFinale.posx());
+  int l = Math.abs(pIniziale.posy() - pIniziale.posy());
+  
+  int x0 = (int) Math.round( h /2.0);
+  int y0 = Math.min(pIniziale.posy(), pFinale.posy());
+  
+  char c;
+  
+  for(int i=0; i<x0; i++) {
+	  for(int j=0; j<l; j++) {
+		  c =canvas.car(x0+i, y0+j);
+		  canvas.modifica(x0+i, y0+j, canvas.car(x0 - i, y0 + j));
+		  canvas.modifica(x0 - i, y0 + j, c);
+		  }
+	  }
   }
-
-  public void azione() {
-
-  }
-
 }
+
+
