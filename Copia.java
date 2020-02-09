f9e4d0344cbf23dade02fbe31d87ce3cc4faa92c
patriziafornoni <60657220+patriziafornoni@users.noreply.github.com>
2020-02-09 18:00:01 +0100

    copia

--- Copia.java
+++ Copia.java
@@ -6,11 +6,18 @@ public class Copia extends StrumentoDiSelezione {
   }
 
   //METODI
-  public void ricevi(EventoDiMouse e) {
-
-  }
-
+  
   public void azione() {
+	  int x0 = Math.max(pIniziale.posx(), pFinale.posx());
+	  int y0 = Math.min(pIniziale.posy(), pFinale.posy());
+	  int h = Math.abs(pIniziale.posx() - pFinale.posx());
+	  int l = Math.abs(pIniziale.posy() - pFinale.posy());
+	  
+	  for (int i = 0; i < h; i++) {
+		  for (int j = 0; j < l ; j++) {
+			  canvas.modifica(actionPoint.posx() - i, actionPoint.posy() + j, canvas.car(x0 - i,y0 + j));
+		  }
+	  }
 
   }
 
